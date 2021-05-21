58f4cdc27419ff2885a323516a7d981da3a09ff5
salvatore <salvatore.la.spata@gmail.com>
2021-05-21 15:49:21 +0200

    up

--- src/components/customers/Customers.tsx
+++ src/components/customers/Customers.tsx
@@ -1,38 +1,9 @@
 import * as React from "react";
-import Paper from "@material-ui/core/Paper";
-import { Column } from "@devexpress/dx-react-grid";
-import {
-   Grid,
-   Table,
-   TableHeaderRow,
-} from "@devexpress/dx-react-grid-material-ui";
-import { generateRows } from "./generate";
-
-// interface IRow {
-//    name: string;
-//    gender: string;
-//    city: string;
-//    car: string;
-// }
-
-const rows = generateRows({ length: 5 });
-const columns: Column[] = [
-   { name: "name", title: "Name" },
-   { name: "gender", title: "Gender" },
-   { name: "city", title: "City" },
-   { name: "car", title: "Car" },
-];
-const tableColumnExtensions: Table.ColumnExtension[] = [
-   { columnName: "gender", width: 100 },
-];
 
 const Customers: React.FC = () => {
-   return (
-      <Grid rows={rows} columns={columns}>
-         <Table columnExtensions={tableColumnExtensions} />
-         <TableHeaderRow />
-      </Grid>
-   );
+    return (
+        <div></div>
+    );
 };
 
 export default Customers;
--- src/pages/Management.tsx
+++ src/pages/Management.tsx
@@ -8,94 +8,82 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Customers from "../components/customers/Customers";
 import Project from "../components/project/Project";
 import AddIcon from '@material-ui/icons/Add';
+import CollapsibleTable from '../components/Table/CollapsibleTable';
 //si andranno a gestire tutti i flussi di anagrafica
 const Management = () => {
-   const domRef = useRef(null);
-   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-   const classes = useStyles();
+    const domRef = useRef(null);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const classes = useStyles();
 
-   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-      setAnchorEl(event.currentTarget);
-   };
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-   const handleClose = () => {
-      setAnchorEl(null);
-   };
-   return (
-      <>
-         <StandardContainer>
-            <Grid item xs={12} md={6} lg={6}>
-               <Paper className={clsx(classes.paper)}>
-                  customers
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <>
+            <StandardContainer>
+                <Grid item xs={12} md={6} lg={6}>
+                    <Paper className={clsx(classes.paper)}>
+                        customers
                   <div>
-                     <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                        Open Menu
-                  </Button>
-                     <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                     >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                        <MenuItem onClick={handleClose}>Logout</MenuItem>
-                     </Menu>
-                  </div>
-                  <Customers />
-               </Paper>
-            </Grid>
+                            <CollapsibleTable />
+                        </div>
+                        <Customers />
+                    </Paper>
+                </Grid>
 
-            <Grid item xs={12} md={6} lg={6}>
-               <Paper className={clsx(classes.paper)}>
-                  project
+                <Grid item xs={12} md={6} lg={6}>
+                    <Paper className={clsx(classes.paper)}>
+                        project
                   <Project />
-               </Paper>
-            </Grid>
-            <Grid item xs={12} md={12} lg={12}>
-               <Paper className={clsx(classes.paper)}>activity</Paper>
-            </Grid>
-         </StandardContainer>
-         <Fab
-            size="medium"
-            color="primary"
-            aria-label="Add Management"
-            className={classes.fabMargin}
-            onClick={handleClick}
-         //gestire menù per selezionare la tipologia di inserimento ( commessa(admin), progetto(team leader), attività, task )
-         >
-            <AddIcon />
-         </Fab>
-         <div ref={domRef}>
-            <Menu
-               anchorEl={anchorEl}
-               keepMounted
-               open={Boolean(anchorEl)}
-               onClose={handleClose}
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={12} lg={12}>
+                    <Paper className={clsx(classes.paper)}>activity</Paper>
+                </Grid>
+            </StandardContainer>
+            <Fab
+                size="medium"
+                color="primary"
+                aria-label="Add Management"
+                className={classes.fabMargin}
+                onClick={handleClick}
+            //gestire menù per selezionare la tipologia di inserimento ( commessa(admin), progetto(team leader), attività, task )
             >
-               <MenuItem onClick={handleClose}>
-                  <div>
-                     <AddIcon color="primary" />
+                <AddIcon />
+            </Fab>
+            <div ref={domRef}>
+                <Menu
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <div>
+                            <AddIcon color="primary" />
                   customer
                </div>
-               </MenuItem>
-               <MenuItem onClick={handleClose}>
-                  <div>
-                     <AddIcon color="primary" />
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                        <div>
+                            <AddIcon color="primary" />
                   project
                </div>
-               </MenuItem>
-               <MenuItem onClick={handleClose}>
-                  <div>
-                     <AddIcon color="primary" />
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                        <div>
+                            <AddIcon color="primary" />
                   activity
                </div>
-               </MenuItem>
-            </Menu>
-         </div>
-      </>
-   );
+                    </MenuItem>
+                </Menu>
+            </div>
+        </>
+    );
 };
 
 export default Management;
